062320067c160b2fd1801993deb7c04cf34e79a9
Alex-gikungu <alexigikungu.012@gmail.com>
2023-06-15 13:20:07 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,25 @@
+// describe("index.js", () => {
+//     it("has a function expression called divide", () => {
+//       expect(divide).to.exist
+//     })
+
+const divide =function (a,b) {
+    return a,b=2000/100;
+}
+ const result= divide(2000/100);
+  console.log(result);
+
+const square =(parameter)=>{
+    let x = 2 ;
+    return parameter * x ;
+}
+const results =square (2);
+console.log(results);
+
+const add = (a,b) =>{
+    return a+b
+
+}
+
+const total= add(3,4)
+console.log(total);
